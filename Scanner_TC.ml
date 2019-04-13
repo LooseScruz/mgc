dde5d33d70cffed9492e47cbb3ef21505c39af90
Mackenzie Glynn <mail@mglynn.net>
2019-04-13 14:24:30 -0700

    A bit of cleanup

--- Scanner_TC.ml
+++ Scanner_TC.ml
@@ -6,16 +6,16 @@ type tc = {
 let create () = { last_tab_count = 0; current_tab_count = 0 }
 
 let incr_prev_tab_count tc = 
-	tc.last_tab_count <- tc.last_tab_count + 1;;
+	tc.last_tab_count <- tc.last_tab_count + 1
 	(* { current_tab_count = tc.current_tab_count + 1; last_tab_count = tc.last_tab_count } *)
 
 let incr_current_tab_count tc = 
-	tc.current_tab_count <- tc.current_tab_count + 1;;
+	tc.current_tab_count <- tc.current_tab_count + 1
 
 let decr_prev_tab_count tc = 
-	tc.last_tab_count <- tc.last_tab_count - 1;;
+	tc.last_tab_count <- tc.last_tab_count - 1
 	(* { current_tab_count = tc.current_tab_count; last_tab_count = tc.last_tab_count - 1 } *)
 
 let adv_tab_count tc =
 	(* { last_tab_count = tc.current_tab_count; current_tab_count = 0 } *)
-	tc.last_tab_count <- tc.current_tab_count; tc.current_tab_count <- 0;;
+	tc.last_tab_count <- tc.current_tab_count; tc.current_tab_count <- 0
